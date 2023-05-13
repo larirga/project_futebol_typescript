bb0b52d907515cb4abe428d0ac7fe864c8ccb769
Larissa <larisssa_rga@hotmail.com>
2023-05-13 10:15:49 -0300

    [add] - /GET teams by id controller and service

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -4,5 +4,6 @@ import TeamController from '../controllers/Team.controller';
 const teamRouter = Router();
 
 teamRouter.get('/', TeamController.getAll);
+teamRouter.get('/:id', TeamController.getById);
 
 export default teamRouter;
--- app/backend/src/services/team.service.ts
+++ app/backend/src/services/team.service.ts
@@ -1,7 +1,16 @@
+import NotFoundError from '../utils/NotFoundError';
 import TeamModel, { TeamAttributes } from '../database/models/Team.model';
 
 export default class TeamService {
   public static async getAll(): Promise<TeamAttributes[]> {
     return TeamModel.findAll();
+  }
+
+  public static async getById(id: number): Promise<TeamAttributes> {
+    const getId = await TeamModel.findByPk(id);
+
+    if (!getId) throw new NotFoundError('There is no team with this id');
+
+    return getId;
   }
 }
